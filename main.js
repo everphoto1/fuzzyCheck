630e4fc71f9a57ade5c881a5d7882682e7e1a29b
everphoto1 <86282018+everphoto1@users.noreply.github.com>
2026-01-19 22:19:20 +0800

    Update API endpoints and request body for checkin

--- main.js
+++ main.js
@@ -4,24 +4,24 @@ const glados = async () => {
   try {
     const headers = {
       'cookie': cookie,
-      'referer': 'https://glados.rocks/console/checkin',
+      'referer': 'https://glados.cloud/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
+    const checkin = await fetch('https://glados.cloud/api/user/checkin', {
       method: 'POST',
-      headers: { ...headers, 'content-type': 'application/json' },
-      body: '{"token":"glados.one"}',
+      headers: { ...headers, 'content-type': 'application/json;charset=UTF-8' },
+      body: JSON.stringify({ token: 'glados.cloud' }),
     }).then((r) => r.json())
 
     if (checkin.code) {
       throw new Error(`API request failed with status: ${checkin.message}`);
     }
-    const status = await fetch('https://glados.rocks/api/user/status', {
+    const status = await fetch('https://glados.cloud/api/user/status', {
       method: 'GET',
       headers,
     }).then((r) => r.json())
 
-     if (status.code) {
+    if (status.code) {
       throw new Error(`API request failed with status: ${status.message}`);
     }
     return [
@@ -30,8 +30,6 @@ const glados = async () => {
       `Left Days ${Number(status.data.leftDays)}`,
     ]
   } catch (error) {
-    throw error
-  
     return [
       'Checkin Error',
       `${error}`,
